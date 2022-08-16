a8bd9cae597ae500d07290f9a3026518b8c468ec
JosiahNonum <106886203+JosiahNonum@users.noreply.github.com>
2022-08-16 18:14:40 -0500

    app.models items update
    added some items (and comments) to the recipe and ingredient models

--- app/models/ingredient.py
+++ app/models/ingredient.py
@@ -4,5 +4,10 @@ from pydantic.types import UUID4
 
 class Ingredient(BaseModel):
     id: UUID4
+    amount: float  # maybe make a way to scale the recipe by multiplying the amount variable?
+    measurementType: str  # it should be possible to leave this blank, i.e. 1 ___ lemon vs 1 tsp cumin
+    # ^^^^^ maybe a shorter variable name?
+    title: str
+
     # todo, write some code here
     #  Add items here to make the model. What makes up an ingredient? A name? An amount? A measurement type?
--- app/models/recipe.py
+++ app/models/recipe.py
@@ -1,6 +1,7 @@
 import uuid
 from pydantic import BaseModel, root_validator
 from pydantic.types import UUID4
+# maybe "from ingredient import Ingredient"
 
 
 class Recipe(BaseModel):
@@ -8,18 +9,17 @@ class Recipe(BaseModel):
     name: str
     recipeYield: str
     description: str
-    ingredientList: str
+    ingredientList: str  # I assume this should be integrated with the ingredient model?
 
     """
-        i'll leave the list at that, but there are other possible items such as:
+        i'll leave the list at that for now but there are other possible items such as:
         actual cooking instructions for how to prepare the dish
         nutritional information
         cooking time and temperature possibly broken up further into prep time, cooking time, total time, 
             and any special times such as rising or marinating
         possible variations or substitutions
-        general post recipe notes to complement the description
+        notes or tips to complement the description
      """
-
 
     # todo, write some code here
     #  Add items here to make the model. What makes up a recipe? Temperature in the oven as a float?
