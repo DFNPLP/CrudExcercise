0bc2d1fad313c37c011b4d058a9608ffbbe1a034
JosiahNonum <106886203+JosiahNonum@users.noreply.github.com>
2022-08-16 17:37:28 -0500

    Update recipe.py
    updated the list of items inside the Recipe class inside app.models.recipe

--- app/models/recipe.py
+++ app/models/recipe.py
@@ -6,7 +6,20 @@ from pydantic.types import UUID4
 class Recipe(BaseModel):
     id: UUID4
     name: str
+    recipeYield: str
     description: str
+    ingredientList: str
+
+    """
+        i'll leave the list at that, but there are other possible items such as:
+        actual cooking instructions for how to prepare the dish
+        nutritional information
+        cooking time and temperature possibly broken up further into prep time, cooking time, total time, 
+            and any special times such as rising or marinating
+        possible variations or substitutions
+        general post recipe notes to complement the description
+     """
+
 
     # todo, write some code here
     #  Add items here to make the model. What makes up a recipe? Temperature in the oven as a float?
