fb6fa6de1e8f80472a25e2a6a2d5f4f73f907e29
JosiahNonum <106886203+JosiahNonum@users.noreply.github.com>
2022-08-17 20:53:34 -0500

    Update recipe.py

--- app/models/recipe.py
+++ app/models/recipe.py
@@ -1,8 +1,11 @@
 import uuid
+from typing import List
+
 from pydantic import BaseModel, root_validator
 from pydantic.types import UUID4
 # maybe "from ingredient import Ingredient" or is it "from app.models.ingredient import Ingredient"?
 # not sure how many folders back I need to go, if any, since the Recipe and Ingredient classes are in the same folder
+from app.models.ingredient import Ingredient
 
 
 class Recipe(BaseModel):
@@ -10,17 +13,9 @@ class Recipe(BaseModel):
     name: str
     recipeYield: str
     description: str
-    ingredientList: str  # I assume this should be integrated with the ingredient model?
+    ingredient_list: List[Ingredient]  # I assume this should be integrated with the ingredient model?
 
-    """
-        i'll leave the list at that for now but there are other possible items such as:
-        actual cooking instructions for how to prepare the dish
-        nutritional information
-        cooking time and temperature possibly broken up further into prep time, cooking time, total time, 
-            and any special times such as rising or marinating
-        possible variations or substitutions
-        notes or tips to complement the description
-     """
+
 
     # todo, write some code here
     #  Add items here to make the model. What makes up a recipe? Temperature in the oven as a float?
